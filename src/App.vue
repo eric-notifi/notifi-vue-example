144fecf8b0297b5912afcd4fd545748a1f9bcf78
Eric Lee <macbookpro4eric@Mac-mini.local>
2023-07-05 14:53:51 +0900

    Init project with dummy card

---
+++ src/App.vue
@@ -0,0 +1,49 @@
+<script lang="ts">
+// import HelloWorld from "./components/HelloWorld.vue";
+// import TheWelcome from "./components/TheWelcome.vue";
+import { NotifiSubscriptionCard, NotifiContext } from "@notifi-network/notifi-react-card";
+import { applyReactInVue, applyPureReactInVue } from "veaury";
+
+export default {
+  components: {
+    Card: applyPureReactInVue(NotifiSubscriptionCard),
+    Context: applyReactInVue(NotifiContext),
+  },
+};
+</script>
+
+<template>
+  <main>
+    <TheWelcome />
+    <Context> <Card /> </Context>
+  </main>
+</template>
+
+<style scoped>
+header {
+  line-height: 1.5;
+}
+
+.logo {
+  display: block;
+  margin: 0 auto 2rem;
+}
+
+@media (min-width: 1024px) {
+  header {
+    display: flex;
+    place-items: center;
+    padding-right: calc(var(--section-gap) / 2);
+  }
+
+  .logo {
+    margin: 0 2rem 0 0;
+  }
+
+  header .wrapper {
+    display: flex;
+    place-items: flex-start;
+    flex-wrap: wrap;
+  }
+}
+</style>
---
+++ src/main.ts
@@ -0,0 +1,15 @@
+// import './assets/main.css'
+
+// import { createApp } from 'vue'
+// import App from './App.vue'
+
+// createApp(App).mount('#app')
+
+import "./assets/main.css";
+import "@notifi-network/notifi-react-card/dist/index.css";
+
+import { createApp } from "vue";
+import App from "./App.vue";
+const app = createApp(App);
+
+app.mount("#app");
